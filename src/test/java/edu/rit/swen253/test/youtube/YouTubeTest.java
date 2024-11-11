9950ab2546798bd4aa2db19cd73abdb807d4d123
Swapnil K Acharjee <acharjee.swapnil@gmail.com>
2024-11-10 20:52:52 -0500

    changed to be ordered

--- src/test/java/edu/rit/swen253/test/youtube/YouTubeTest.java
+++ src/test/java/edu/rit/swen253/test/youtube/YouTubeTest.java
@@ -5,35 +5,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.logging.Logger;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import edu.rit.swen253.page.SimplePage;
 import edu.rit.swen253.page.youtube.*;
 import edu.rit.swen253.test.AbstractWebTest;
 
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class YouTubeTest extends AbstractWebTest {
     // Logger object to display titles and urls of search results
     private static final Logger logger = Logger.getLogger(YouTubeTest.class.getName());
 
+    private YouTubeHomePage homepage;
+    private YouTubeResultsPage resultsPage;
 
     @Test
-    public void testSearch() {
+    @Order(1)
+    public void navToHome() {
         // Nav to home page
-        YouTubeHomePage homepage = navigateToPage("https://www.youtube.com/", YouTubeHomePage::new); 
+        homepage = navigateToPage("https://www.youtube.com/", YouTubeHomePage::new); 
+    }
 
-        // Search for "The Egg - A Short Story"
-        homepage.search("\"The Egg - A Short Story\" kurzgesagt");
-
-        // Transition to ResultsPage
+    @Test
+    @Order(2)
+    public void search() {
+        // Search for "page object model"
+        homepage.search("page object model");
         homepage.waitUntilGone();
-        YouTubeResultsPage resultsPage = assertNewPage(YouTubeResultsPage::new);
+    }
+
+    @Test
+    @Order(3)
+    public void getResults() {
+        resultsPage = assertNewPage(YouTubeResultsPage::new);
 
         // Get each result and print it out
         for (YouTubeResultView result : resultsPage.getResults()) {
             logger.info(String.format("{Title: %s; Link: %s}", result.getTitle(), result.getLink()));
         }
+    }
 
+    @Test
+    @Order(4)
+    public void navToResult() {
         // Transition to result
         resultsPage.getResults().get(0).followResult();
         resultsPage.waitUntilGone();
@@ -43,8 +60,7 @@ public class YouTubeTest extends AbstractWebTest {
         final SimplePage resultPage = assertNewPage(SimplePage::new);
         assertAll("Page Assertions",
             () -> assertEquals("YouTube", resultPage.getTitle()),
-            () -> assertEquals("https://www.youtube.com/watch?v=h6fcK_fRYaI", resultPage.getURL())
+            () -> assertEquals("https://www.youtube.com/watch?v=GKDJk4s_T-s", resultPage.getURL())
         );
     }
-
 }
